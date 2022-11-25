4c78bcef46edfebe85c4f0ff69d788e45a4cf5c0
Jeremy Pichon <jds.pichon@gmail.com>
2022-11-25 22:32:59 +0900

    /projects - Add content

--- apps/portal-web/app/(protected)/projects/page.tsx
+++ apps/portal-web/app/(protected)/projects/page.tsx
@@ -1,6 +1,95 @@
+'use client';
+
+import React, { useEffect, useState } from 'react';
+
+import { ApiConnector } from '@locale-hub/api-connector';
+import { MeDashboardResponse, Project } from '@locale-hub/data';
+import { Button, DateFormat, Menu, Modal, Table } from '@locale-hub/design-system';
+import { EllipsisHorizontalIcon } from '@heroicons/react/24/outline';
+import Link from 'next/link';
+import { routes } from '../../../constants/routes';
+import ProgressBar from '../../../../../libs/design-system/src/lib/progress-bar/progress-bar';
 
 export default function ProjectsPage() {
-  return <div className="">
-    <h1>Projects</h1>
-  </div>;
+  const [data, setData] = useState<MeDashboardResponse>(null);
+  const [deleteModal, setDeleteModal] = useState(false);
+
+  useEffect(() => {
+    ApiConnector.me.dashboard().then(data => {
+      if ('error' in data) {
+        // TODO Toast
+        return;
+      }
+      setData(data);
+    })
+  }, []);
+
+  const projectName = (project: Project) => {
+    const organizationName = data.organizations.find(org => org.id === project.organizationId).name;
+    return <>
+      <Link className='text-primary' href={`${routes.organizations}/${project.organizationId}`}>{organizationName}</Link>
+      <span className='px-1'>/</span>
+      <Link className='text-primary' href={`${routes.projects}/${project.id}`}>{project.name}</Link>
+    </>;
+  }
+  const projectProgress = (projectId: string) => {
+    const progress = 100 * data.progress.find(project => project.projectId === projectId).progress;
+    return <ProgressBar fill={progress} />;
+  }
+
+  const deleteProject = (projectId: string) => {
+    useEffect(() => {
+      ApiConnector.projects.delete(projectId).then(res => {
+        if ('error' in res) {
+          // TODO Toast
+          return;
+        }
+        // TODO Toast
+        data.projects = data.projects.filter(p => p.id !== projectId);
+        setData(data);
+      })
+    }, []);
+  };
+
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState<React.ReactNode>(null);
+  const [actions, setActions] = useState<React.ReactNode>(null);
+  const openDeletionModalFor = (project: Project) => {
+    setTitle('Are you sure?');
+    setContent(<>
+      <p>
+        This action cannot be undone.<br/>
+        This will permanently delete the <b className='text-warn'>{ project.name }</b> project and all it commits and entries.<br/>
+      </p>
+    </>);
+    setActions(<>
+      <Button type='cancel' onClick={() => setDeleteModal(false)}>Cancel</Button>
+      <Button type='confirm' onClick={() => deleteProject(project.id)}>Confirm</Button>
+    </>);
+    setDeleteModal(true);
+  };
+
+  return <>
+    <Modal isOpen={deleteModal} title={title} content={content} actions={actions} onClose={() => setDeleteModal(false)} />
+    <Table
+      className='w-7/12 mx-auto mt-12'
+      heads={[
+        { key: 'name', label: 'Project Name', className: 'w-7/12' },
+        { key: 'status', label: 'Status', className: 'w-2/12' },
+        { key: 'createdAt', label: 'Creation Date', className: 'w-2/12' },
+        { key: 'actions', label: 'Actions', className: 'w-1/12' }
+      ]}
+      entries={data?.projects.map(project => ({
+        name: projectName(project),
+        status: projectProgress(project.id),
+        createdAt: <DateFormat date={project.createdAt} />,
+        actions: <Menu
+          onClick={() => openDeletionModalFor(project) }
+          button={<EllipsisHorizontalIcon className='h-6 w-6 text-white' />}
+        >
+          <span className='text-warn'>Delete</span>
+        </Menu>
+      })) ?? []}
+    />
+  </>;
 }
