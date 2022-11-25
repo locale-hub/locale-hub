ebd68db98495e67101136c1a63cba8b91f4ecdb3
Jeremy Pichon <jds.pichon@gmail.com>
2022-11-25 23:15:23 +0900

    /organizations - page added

--- apps/portal-web/app/(protected)/organizations/page.tsx
+++ apps/portal-web/app/(protected)/organizations/page.tsx
@@ -1,6 +1,35 @@
+'use client';
+
+import React, { useEffect, useState } from 'react';
+
+import { ApiConnector } from '@locale-hub/api-connector';
+import { MeDashboardResponse, } from '@locale-hub/data';
+import { DateFormat, Table } from '@locale-hub/design-system';
+import { routes } from '../../../constants/routes';
+import Link from 'next/link';
 
 export default function OrganizationsPage() {
-  return <div className="">
-    <h1>Organizations</h1>
-  </div>;
+  const [data, setData] = useState<MeDashboardResponse>(null);
+
+  useEffect(() => {
+    ApiConnector.me.dashboard().then(data => {
+      if ('error' in data) {
+        // TODO Toast
+        return;
+      }
+      setData(data);
+    })
+  }, []);
+
+  return <Table
+    className='w-6/12 mx-auto mt-12'
+    heads={[
+      { key: 'name', label: 'Project Name', className: 'w-3/4' },
+      { key: 'createdAt', label: 'Creation Date', className: 'w-1/4' },
+    ]}
+    entries={data?.organizations.map(organization => ({
+      name: <Link className='text-primary' href={`${routes.organizations}/${organization.id}`}>{organization.name}</Link>,
+      createdAt: <DateFormat date={organization.createdAt} />
+    })) ?? []}
+  />;
 }
