7fc997e52121477632a382e9d6542d0c6405640d
Jeremy Pichon <jds.pichon@gmail.com>
2022-12-06 22:13:15 +0900

    🚧 WIP Project Translation page - Translation editor apply changes to manifest

--- libs/design-system/src/lib/button/button.tsx
+++ libs/design-system/src/lib/button/button.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 
-
-
 export default function Button({
   children,
   disabled,
@@ -10,7 +8,7 @@ export default function Button({
 }: {
   children?: React.ReactNode,
   disabled?: boolean,
-  onClick: () => void
+  onClick: React.MouseEventHandler<HTMLButtonElement>
   type?: 'action' | 'default' | 'cancel' | 'confirm'
 }) {
   type ??= 'default';
