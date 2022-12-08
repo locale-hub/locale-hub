a8cf9c08801fde7028e71098078e751be4f389c8
Jeremy Pichon <jds.pichon@gmail.com>
2022-12-08 21:44:06 +0900

    🚧 WIP Project Translation page - Add locale button

---
+++ apps/portal-web/app/(protected)/projects/[projectId]/translations/add-locale-modal.tsx
@@ -0,0 +1,46 @@
+import { Dialog } from '@headlessui/react';
+import React, { useState } from 'react';
+import { Button, Select } from '@locale-hub/design-system';
+import { locales } from '../../../../../constants/locales';
+
+export default function AddLocaleModal({
+  isOpen,
+  onClose
+}: {
+  isOpen: boolean,
+  onClose: (localeTag?: string) => void,
+}) {
+  const [locale, setLocale] = useState<string>(locales[0].tag);
+
+  // TODO: Add file import feature
+
+  return <Dialog as="div" className="relative" open={isOpen} onClose={() => onClose(locale)}>
+    <div className="fixed z-40 inset-0 bg-black bg-opacity-25 dark:bg-opacity-50" />
+    <div className="fixed z-50 inset-0 overflow-y-auto">
+      <div className="flex min-h-full items-center justify-center p-4 text-center">
+          <Dialog.Panel className="w-2xl h-2xl transform rounded-2xl bg-white dark:bg-slate-800 p-6 text-left align-middle shadow-xl transition-all">
+            <Dialog.Title as="h2"
+              className="mb-4 text-lg font-medium leading-6 text-gray-900 dark:text-white"
+            >
+              Add new locale
+            </Dialog.Title>
+
+            <div className="mt-2 text-black">
+              <div className='w-96'>
+                <Select label='Locale'
+                  onSelect={(value) => setLocale(value.id)}
+                  defaultSelected={{ id: locales[0].tag, value: `${locales[0].tag} - ${locales[0].name}` }}
+                  values={locales.map(locale => ({ id: locale.tag, value: `${locale.tag} - ${locale.name}` }))}
+                />
+              </div>
+            </div>
+
+            <div className="mt-8 text-black text-right">
+              <Button type='cancel' onClick={() => onClose()}>Cancel</Button>
+              <Button type='confirm' onClick={() => onClose(locale)}>Save</Button>
+            </div>
+          </Dialog.Panel>
+      </div>
+    </div>
+  </Dialog>;
+}
