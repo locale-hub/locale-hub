3d7cebe9ae794d36fe213a2e3c27dd641553ad60
Jeremy Pichon <jds.pichon@gmail.com>
2022-12-09 23:17:11 +0900

    Add Create project/organization modals

---
+++ apps/portal-web/components/add-organization-modal.tsx
@@ -0,0 +1,41 @@
+import { Dialog } from '@headlessui/react';
+import React, { useState } from 'react';
+import { Button, InputField, Select } from '@locale-hub/design-system';
+import { locales } from '../constants/locales';
+import { Organization } from '@locale-hub/data';
+
+export default function AddOrganizationModal({
+  isOpen,
+  onClose,
+}: {
+  isOpen: boolean,
+  onClose: (orgName?: string) => void,
+}) {
+  const [orgName, setOrgName] = useState<string>('');
+
+  return <Dialog as="div" className="relative" open={isOpen} onClose={() => onClose(orgName)}>
+    <div className="fixed z-40 inset-0 bg-black bg-opacity-25 dark:bg-opacity-50" />
+    <div className="fixed z-50 inset-0 overflow-y-auto">
+      <div className="flex min-h-full items-center justify-center p-4 text-center">
+          <Dialog.Panel className="w-2xl h-2xl transform rounded-2xl bg-white dark:bg-slate-800 p-6 text-left align-middle shadow-xl transition-all">
+            <Dialog.Title as="h2"
+              className="mb-4 text-lg font-medium leading-6 text-gray-900 dark:text-white"
+            >
+              Create new Organizatiom
+            </Dialog.Title>
+
+            <div className="mt-2 text-black">
+              <div className='w-96'>
+                <InputField name={'organization-name'} label={'Organization Name'} onValue={setOrgName} type={'text'} value={orgName} placeholder='Organization Name' />
+              </div>
+            </div>
+
+            <div className="mt-8 text-black text-right">
+              <Button type='cancel' onClick={() => onClose()}>Cancel</Button>
+              <Button type='confirm' onClick={() => onClose(orgName)}>Create</Button>
+            </div>
+          </Dialog.Panel>
+      </div>
+    </div>
+  </Dialog>;
+}
--- apps/portal-web/components/navigation-authenticated.tsx
+++ apps/portal-web/components/navigation-authenticated.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useEffect, useState } from 'react';
+import React, { Children, useEffect, useState } from 'react';
 import { BellAlertIcon, DocumentTextIcon, PlusIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 
@@ -8,13 +8,19 @@ import { routes } from '../constants/routes';
 import { SlideOver, Spacer, UserIcon } from '@locale-hub/design-system';
 import { useAuth } from '../contexts/AuthContext';
 import { ApiConnector } from '@locale-hub/api-connector';
-import { Notification, NotificationStatus } from '@locale-hub/data';
+import { Notification, NotificationStatus, Organization } from '@locale-hub/data';
 import NotificationCard from './notification-card';
+import { Menu } from '@headlessui/react';
+import AddProjectModal from './add-project-modal';
+import AddOrganizationModal from './add-organization-modal';
 
 export default function NavigationAuthenticated() {
   const { user, logout } = useAuth();
   const [openNotifications, setOpenNotifications] = useState(false);
+  const [openAddProject, setOpenAddProject] = useState(false);
+  const [openAddOrganization, setOpenAddOrganization] = useState(false);
   const [notifications, setNotifications] = useState<Notification[]>([]);
+  const [organizations, setOrganizations] = useState<Organization[]>([]);
 
   useEffect(() => {
     ApiConnector.notifications.list().then(data => {
@@ -23,6 +29,13 @@ export default function NavigationAuthenticated() {
         return;
       }
       setNotifications(data.notifications);
+    });
+    ApiConnector.organizations.list().then(data => {
+      if ('error' in data) {
+        // TODO: Toast
+        return;
+      }
+      setOrganizations(data.organizations);
     });
   }, []);
 
@@ -36,7 +49,38 @@ export default function NavigationAuthenticated() {
     });
   };
 
+  const onProjectAdd = (orgId: string, projectName: string) => {
+    setOpenAddProject(false);
+    if (undefined === orgId || undefined === projectName) {
+      return;
+    }
+    ApiConnector.projects.post(orgId, projectName, 'en').then(data => {
+      if ('error' in data) {
+        // TODO: Toast
+        return;
+      }
+      // TODO: Toast
+    });
+  }
+  const onOrganizationAdd = (orgName: string) => {
+    setOpenAddOrganization(false);
+    if (undefined === orgName) {
+      return;
+    }
+    ApiConnector.organizations.post(orgName).then(data => {
+      if ('error' in data) {
+        // TODO: Toast
+        return;
+      }
+      // TODO: Toast
+    });
+  }
+
   return <>
+    { 0 !== organizations.length &&
+      <AddProjectModal isOpen={openAddProject} onClose={onProjectAdd} organizations={organizations} />
+    }
+    <AddOrganizationModal isOpen={openAddOrganization} onClose={onOrganizationAdd} />
     <SlideOver isOpen={openNotifications}
        onClose={() => setOpenNotifications(false)}
        title='Notifications'
@@ -48,10 +92,13 @@ export default function NavigationAuthenticated() {
         />)
       }
     </SlideOver>
+
     <Link href={routes.organizations.root} className='px-4'>Organizations</Link>
     <Link href={routes.projects.root} className='px-4'>Projects</Link>
     <Spacer />
+
     <DocumentTextIcon className='mx-2 h-6 w-6 hover:cursor-pointer' />
+
     <div className='relative hover:cursor-pointer'
          onClick={() => setOpenNotifications(!openNotifications)}>
       <div className="inline-flex absolute -top-2 right-0 justify-center items-center w-5 h-5 text-xs font-bold rounded-full bg-primary">
@@ -59,7 +106,22 @@ export default function NavigationAuthenticated() {
       </div>
       <BellAlertIcon className='mx-2 h-6 w-6' />
     </div>
-    <PlusIcon className='mx-2 h-6 w-6 hover:cursor-pointer' />
+
+    <Menu as="div" className={`relative`}>
+      <Menu.Button className="flex max-w-xs m-auto items-center">
+        <span className="sr-only">Open add menu</span>
+        <PlusIcon className='mx-2 h-6 w-6 hover:cursor-pointer' />
+      </Menu.Button>
+      <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md rounded-md py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+        <a onClick={() => setOpenAddProject(true)} className='bg-gray-100 block px-4 py-2 text-black hover:cursor-pointer hover:bg-slate-200'>
+          Project
+        </a>
+        <a onClick={() => setOpenAddOrganization(true)} className='bg-gray-100 block px-4 py-2 text-black hover:cursor-pointer hover:bg-slate-200'>
+          Organization
+        </a>
+      </Menu.Items>
+    </Menu>
+
     <UserIcon name={user?.name} className='ml-3'>
       <Link href={routes.profiles.me} className='bg-gray-100 block px-4 py-2 text-black hover:cursor-pointer hover:bg-slate-200'>Profile</Link>
       <a onClick={logout} className='bg-gray-100 block px-4 py-2 text-warn hover:cursor-pointer hover:bg-slate-200'>Logout</a>
